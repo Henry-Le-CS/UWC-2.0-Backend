dac08a75cc35de96e586599e1dd132f198b1d419
Henry Le <henryle@Hieus-MacBook-Pro.local>
2023-03-31 06:04:56 +0700

    feat: add login and display profile

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,6 +2,8 @@ const express = require("express");
 const userController = require("../controllers/userControllers");
 const app = express();
 const router = express.Router();
+
 router.post("/login",userController.login);
 router.post("/infoBO",userController.infoBO);
+
 module.exports = router;
